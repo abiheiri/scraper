c84568f45b4a85180e184d0a171444157c5dca5e
Al Biheiri <al@forgottheaddress.com>
2022-11-12 16:06:51 -0800

    testing pylint

--- scraper/test_scrape.py
+++ scraper/test_scrape.py
@@ -1,9 +1,8 @@
-import pytest
+"importing scraper"
 from scraper import Scraper
 
 def test_scrape_help():
+    """testing the -h flag"""
     scrape = Scraper()
 
     scrape.parse_args ("-h")
-
-    
