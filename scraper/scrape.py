2baaec1068575157526540f6f61e63acb3879f9d
Al Biheiri <al@forgottheaddress.com>
2023-12-22 22:09:32 -0800

    fixing maxdepth and removing -a flag

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -1,134 +1,66 @@
 #!/usr/bin/env python
 
 from bs4 import BeautifulSoup
-import sys, argparse, requests, re
-
+import requests
+import argparse
+import sys
+from urllib.parse import urljoin
 
 class Scraper:
-    prog='scraper'
-    version='1.0'
-    author='Al Biheiri (al@forgottheaddress.com)'
-    HTTPTimeOutValue=10
+    def __init__(self):
+        self.prog = 'scraper'
+        self.version = '1.0'
+        self.author = 'Al Biheiri (al@forgottheaddress.com)'
+        self.http_timeout = 10
+        self.visited_urls = set()
 
-
-    def do_list(self, fqdn, addURL, maxDepth):
-
-        # Go to the other function instead
-        if maxDepth > 0:
-            self.do_listWithDepth(fqdn, addURL, maxDepth)
-            return
-            
-        page = requests.get(fqdn, timeout=self.HTTPTimeOutValue)
-        bs = BeautifulSoup(page.content, features='lxml')
-
-        for link in bs.findAll('a'):
-            if addURL == 1:
-                print (fqdn + link.get('href'))
-            else:
-                print (link.get('href'))
-                # print("href: {}".format(link.get("href")))
-
-
-    def do_listWithDepth(self, fqdn, addURL, maxDepth):
-    
-        results = []
-        
-        # Get the top level site
-        page = requests.get(fqdn, timeout=self.HTTPTimeOutValue)
-        bs = BeautifulSoup(page.content, features='lxml')
-        for link in bs.findAll('a'):
-            if addURL == 1:
-                try:
-                    results.append(fqdn + link.get('href'))
-                except Exception as e: print(e)
-            else:
-                results.append(link.get('href'))
-
-
-        siteRemaining = len(results)
-
-        for i in results:
-            # print(i)
-            while maxDepth > 0:
-                
-                maxDepth -= 1
-                siteRemaining -=1
-                # print(siteRemaining)
-                
-                try:
-                    page = requests.get(results[siteRemaining], timeout=self.HTTPTimeOutValue)
-                    bs = BeautifulSoup(page.content, features='lxml')
-
-                    for link in bs.findAll('a'):
-                        
-                        # Skip empty results
-                        if link.get('href') is None:
-                            continue
-                        
-                        # Determine if full URL provided in <a href
-                        if link.get('href').find('http') != -1:
-                            results.append(link.get('href'))
-                        else:
-                            results.append(results[siteRemaining] + link.get('href'))
-
-                except(ValueError):
-                    continue
-
+    def fetch_links(self, url):
+        """ Fetch links from a given URL and return full URLs. """
         try:
-            results.sort()
-        except:
-            pass
-        
-        # print(results)
-        # print(type(results))
-        print(*results, sep='\n')
+            response = requests.get(url, timeout=self.http_timeout)
+            response.raise_for_status()
+            soup = BeautifulSoup(response.content, features='lxml')
 
+            links = []
+            for link in soup.findAll('a'):
+                href = link.get('href')
+                if href and not href.startswith('?'):
+                    full_url = urljoin(url, href)
+                    links.append(full_url)
 
+            return links
+        except requests.RequestException as e:
+            print(f"Error fetching {url}: {e}")
+            return []
 
+    def scrape_links(self, url, depth, link_limit=10):
+        """ Recursively scrape links to a specified depth, printing as they are found. """
+        if depth <= 0 or url in self.visited_urls:
+            return
 
+        self.visited_urls.add(url)
+        links = self.fetch_links(url)[:link_limit]
 
+        for link in links:
+            print(link)
+            self.scrape_links(link, depth - 1)
+
+    def run(self, url, max_depth):
+        """ Run the scraper with the given arguments. """
+        if max_depth > 10:
+            print("Depth too large. Limiting to 10 for performance reasons.")
+            max_depth = 10
+
+        self.scrape_links(url, max_depth)
 
     def parse_args(self, args):
-        from argparse import RawTextHelpFormatter
-        parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter, 
-        epilog=\
-        '''
-
-        ----------------------------------------------------------------
-        This is designed to scrape websites and return all the links <a href="http://website"></a> from the page
-        
-        ''',
-        )
-
-
-        parser.add_argument('--version', action='version', version='{} {}'.format(self.prog, self.version))
-
-
-        group = parser.add_mutually_exclusive_group()
-        group.add_argument("-l", dest="list", metavar="http://mywebsite", help="list all the links inside a website")
-        
-        parser.add_argument("-a", dest="add", action="store_true", help="add back the url to the front of each result")
-        parser.add_argument("-m", dest="maxdepth", metavar="NUMBER", help="drill down to a maxdepth of X", type=int)
-
-
-
-        args = parser.parse_args()
-        addURL = 0
-        maxDepth = 0
-
-        if args.list:
-            if args.add:
-                addURL=1
-            if args.maxdepth:
-                maxDepth = args.maxdepth
-                if maxDepth > 10:
-                    print ("Number too large. Your computer will not be able to handle it!")
-                    return
-
-            self.do_list(args.list, addURL, maxDepth)
-
-        else:
-            print(f'Usage: {sys.argv[0]} -h', "for help.")
+        """ Parse command line arguments. """
+        parser = argparse.ArgumentParser(description="Web Scraper")
+        parser.add_argument('--version', action='version', version=f'{self.prog} {self.version}')
+        parser.add_argument("-l", dest="url", help="URL to scrape")
+        parser.add_argument("-m", dest="max_depth", type=int, help="Maximum depth to scrape", default=1)
+        return parser.parse_args(args)
 
 if __name__ == "__main__":
-    Scraper().parse_args(sys.argv[1:])
+    args = Scraper().parse_args(sys.argv[1:])
+    Scraper().run(args.url, args.max_depth)
