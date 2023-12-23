54989ca37597124549fba28c9e707657b689a218
Al Biheiri <al_biheiri@apple.com>
2023-12-23 11:16:42 -0800

    fix the filtering option

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Web Scraper Tool
 
-A simple web scraping tool that extracts links from a given website up to a specified depth.
+A simple web scraping tool that extracts links from a given website up to a specified depth. It now includes functionality to filter the results by file extension.
 
 ## Requirements
 
@@ -11,13 +11,13 @@ A simple web scraping tool that extracts links from a given website up to a spec
 
 Before running the scraper, ensure that you have `pipenv` installed. If you do not have `pipenv`, you can install it using pip:
 
-```bash
+```
 pip install pipenv
 ```
 
 Once pipenv is installed, navigate to the directory containing the Pipfile and set up the environment:
 
-```bash
+```
 # Navigate to the scraper directory (modify according to your actual directory)
 cd path/to/scraper
 
@@ -25,43 +25,50 @@ cd path/to/scraper
 pipenv install
 ```
 
-# Usage
+## Usage
 
 To use the tool, first activate the pipenv shell, then run the following command:
 
-```bash
+```
 # Activate the pipenv environment
 pipenv shell
 
 # Run the scraper
-python scrape.py [url] [-m MAX_DEPTH]
-
+python scrape.py [url] [-m MAX_DEPTH] [-f FILE_EXTENSION]
 ```
 
 ### Arguments and Options
 
-url: The URL of the website you want to scrape. This is a required argument.
+- `url`: The URL of the website you want to scrape. This is a required argument.
+- `-m`, `--max_depth`: Optional. Specifies the maximum depth for scraping. Defaults to 1 if not specified.
+- `-f`, `--filter`: Optional. Filter results by a specific file extension, e.g., 'mkv'.
 
--m, --max_depth: Optional. Specifies the maximum depth for scraping. Defaults to 1 if not specified.
+### Examples
 
-Examples
 Scrape links from a website with the default depth:
 
-```bash
+```
 python scrape.py http://example.com
 ```
 
 Scrape links from a website up to a depth of 3:
-```bash
+
+```
 python scrape.py http://example.com -m 3
 ```
 
-To check the version of the tool, use the --version flag:
+Scrape links from a website, filtering for a specific file extension (e.g., .mkv files):
 
-```bash
+```
+python scrape.py http://example.com -m 3 -f mkv
+```
+
+To check the version of the tool, use the `--version` flag:
+
+```
 python scrape.py --version
 ```
 
-# Author
+## Author
 
 Al Biheiri (al@forgottheaddress.com)
--- scraper/scrape.py
+++ scraper/scrape.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
 
+import signal
+import sys
 from bs4 import BeautifulSoup
 import requests
 import argparse
-import sys
 from urllib.parse import urljoin
 
 class Scraper:
     def __init__(self):
         self.prog = 'scraper'
-        self.version = '1.3'
+        self.version = '1.4'
         self.author = 'Al Biheiri (al@forgottheaddress.com)'
         self.http_timeout = 10
         self.visited_urls = set()
         self.filter_ext = None
+
+    def is_directory_link(self, url):
+        # Simple heuristic: if the URL doesn't have a period in the last segment, it's likely a directory
+        return '/' in url.rstrip('/') and '.' not in url.rstrip('/').split('/')[-1]
 
     def fetch_links(self, url):
         """ Fetch links from a given URL and return full URLs. """
@@ -28,8 +33,7 @@ class Scraper:
                 if href:
                     full_url = urljoin(url, href)
                     if full_url.startswith(('http://', 'https://')):
-                        if not self.filter_ext or full_url.endswith(self.filter_ext):
-                            links.append(full_url)
+                        links.append(full_url)
 
             return links
         except requests.RequestException as e:
@@ -42,11 +46,13 @@ class Scraper:
             return
 
         self.visited_urls.add(url)
-        links = self.fetch_links(url)[:link_limit]
 
+        links = self.fetch_links(url)[:link_limit]
         for link in links:
-            print(link)
-            self.scrape_links(link, depth - 1)
+            if self.filter_ext and link.lower().endswith(self.filter_ext):
+                print(link)
+            if self.is_directory_link(link) or (self.filter_ext and link.lower().endswith(self.filter_ext)):
+                self.scrape_links(link, depth - 1)
 
     def run(self, url, max_depth):
         """ Run the scraper with the given arguments. """
@@ -57,6 +63,7 @@ class Scraper:
             print("Depth too large. Limiting to 10 for performance reasons.")
             max_depth = 10
 
+        print(f"Filter set to: {self.filter_ext}")
         self.scrape_links(url, max_depth)
 
     def parse_args(self, args):
@@ -64,15 +71,22 @@ class Scraper:
         parser = argparse.ArgumentParser(description="Web Scraper")
         parser.add_argument('url', help="URL to scrape")
         parser.add_argument('-m', '--max_depth', type=int, help="Maximum depth to scrape", default=1)
-        parser.add_argument('-f', '--filter', type=str, help="Filter results by file extension, e.g., '.mp4'")
+        parser.add_argument('-f', '--filter', type=str, help="Filter results by file extension, e.g., 'mkv' (without the dot)")
         parser.add_argument('--version', action='version', version=f'{self.prog} {self.version}')
 
         args = parser.parse_args(args)
         if args.filter:
-            self.filter_ext = args.filter.lower()
+            self.filter_ext = '.' + args.filter.lower()  # Ensure dot is included
         return args
 
+def signal_handler(sig, frame):
+    print("Quitting...")
+    sys.exit(0)
+
 if __name__ == "__main__":
+    # Register the signal handler for Ctrl+C
+    signal.signal(signal.SIGINT, signal_handler)
+
     scraper = Scraper()
     args = scraper.parse_args(sys.argv[1:])
     scraper.run(args.url, args.max_depth)
