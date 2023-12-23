fcfc611b639922e98110aa366e498a73a0f13aba
Al Biheiri <al@forgottheaddress.com>
2023-12-22 22:27:29 -0800

    fix the test script

--- scraper/test_scrape.py
+++ scraper/test_scrape.py
@@ -1,8 +1,12 @@
-"importing scraper"
 from scraper import Scraper
+import pytest
 
 def test_scrape_help():
     """testing the -h flag"""
     scrape = Scraper()
 
-    scrape.parse_args ("-h")
+    with pytest.raises(SystemExit) as e:
+        scrape.parse_args("-h")
+    
+    assert e.type == SystemExit
+    assert e.value.code == 0  # Ensure that the exit code is 0, which typically indicates a clean exit
